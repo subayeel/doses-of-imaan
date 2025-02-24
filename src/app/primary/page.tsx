71e2203f72f563f0defd3c72dc0aadee28b8f96e
subayeel <abdullahsubayeel999@gmail.com>
2025-02-25 00:26:23 +0530

    fix:resoled build issue

--- src/app/primary/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { ReactNode } from "react";
-
-function Page({ children }: { children: ReactNode }) {
-  return <main className="px-6 py-4">{children}</main>;
-}
-
-export default Page;
--- src/app/self-help/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { ReactNode } from "react";
-
-const Page = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
-};
-
-export default Page;
